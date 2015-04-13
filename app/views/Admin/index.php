3ece9592602649144b1c7710cec045f7fa07f21f
manthan787 <manthant15@gmail.com>
2015-04-13 18:23:22 +0530

    Path Updated

--- app/views/Admin/index.php
+++ app/views/Admin/index.php
@@ -2,7 +2,7 @@
 <head>
         <meta charset="utf-8">
         <title>Admin</title>
-        <link rel="stylesheet" href="/app/admin/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="public/app/admin/css/bootstrap.min.css" />
         <link rel="stylesheet" href="/app/admin/css/font-awesome.min.css" />
         <link rel="stylesheet" href="/app/admin/css/animate/animate.min.css" />
         <link rel="stylesheet" href="/app/admin/css/iCheck/all.css" />
@@ -17,13 +17,13 @@
 
         <!-- Interface -->
         <base href="http://54.149.64.181/public"
-        <script src="/app/admin/js/plugins/jquery/jquery-1.10.2.min.js" type="text/javascript"></script>
-        <script src="/app/admin/js/plugins/jquery-ui/jquery-ui-1.10.4.min.js" type="text/javascript"></script>
+        <script src="public/app/admin/js/plugins/jquery/jquery-1.10.2.min.js" type="text/javascript"></script>
+        <script src="public/app/admin/js/plugins/jquery-ui/jquery-ui-1.10.4.min.js" type="text/javascript"></script>
 
-        <script src="/app/admin/js/plugins/bootstrap/bootstrap.min.js" type="text/javascript"></script>
+        <script src="public/app/admin/js/plugins/bootstrap/bootstrap.min.js" type="text/javascript"></script>
 
-        <script src="/app/admin/js/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-        <script src="/app/admin/js/custom.js" type="text/javascript"></script>
+        <script src="public/app/admin/js/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+        <script src="public/app/admin/js/custom.js" type="text/javascript"></script>
 
 
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.5/angular.min.js"></script>
