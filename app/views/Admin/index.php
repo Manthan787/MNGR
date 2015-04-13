509721c8b97b2eabb3bf3bd00cf2bf0d53e5d5e8
manthan787 <manthant15@gmail.com>
2015-04-13 18:26:30 +0530

    Path Updated

--- app/views/Admin/index.php
+++ app/views/Admin/index.php
@@ -16,35 +16,35 @@
         </div>
 
         <!-- Interface -->
-        <base href="http://54.149.64.181/public"
-        <script src="public/app/admin/js/plugins/jquery/jquery-1.10.2.min.js" type="text/javascript"></script>
-        <script src="public/app/admin/js/plugins/jquery-ui/jquery-ui-1.10.4.min.js" type="text/javascript"></script>
 
-        <script src="public/app/admin/js/plugins/bootstrap/bootstrap.min.js" type="text/javascript"></script>
+        <script src="app/admin/js/plugins/jquery/jquery-1.10.2.min.js" type="text/javascript"></script>
+        <script src="app/admin/js/plugins/jquery-ui/jquery-ui-1.10.4.min.js" type="text/javascript"></script>
 
-        <script src="public/app/admin/js/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-        <script src="public/app/admin/js/custom.js" type="text/javascript"></script>
+        <script src="app/admin/js/plugins/bootstrap/bootstrap.min.js" type="text/javascript"></script>
+
+        <script src="app/admin/js/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+        <script src="app/admin/js/custom.js" type="text/javascript"></script>
 
 
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.5/angular.min.js"></script>
         <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-route.js"></script>
-        <script src="/app/app.js"> </script>
-        <script src="/app/Questions/Questions.js"></script>
-        <script src="/app/Questions/Questions.factory.js"></script>
-        <script src="/app/app.js"> </script>
-        <script src="/app/Students/Students.js"></script>
-        <script src="/app/Students/Students.factory.js"></script>
-        <script src="/app/Services/SchoolFinder.js"></script>
-        <script src="/app/Settings/Settings.js"></script>
-        <script src="/app/Settings/StandardService.js"></script>
-        <script src="/app/Settings/StaffController.js"></script>
-        <script src="/app/User/User.js"></script>
-        <script src="/app/Students/FormHelper.js"></script>
-        <script src="/app/Auth/Auth.js"></script>
-        <script src="/app/Auth/Authaid.js"></script>
-        <script src="/app/Auth/AuthService.js"></script>
-        <script src="/app/Panel.directive.js"></script>
-        <script src="/app/HomeController.js"></script>
+        <script src="app/app.js"> </script>
+        <script src="app/Questions/Questions.js"></script>
+        <script src="app/Questions/Questions.factory.js"></script>
+        <script src="app/app.js"> </script>
+        <script src="app/Students/Students.js"></script>
+        <script src="app/Students/Students.factory.js"></script>
+        <script src="app/Services/SchoolFinder.js"></script>
+        <script src="app/Settings/Settings.js"></script>
+        <script src="app/Settings/StandardService.js"></script>
+        <script src="app/Settings/StaffController.js"></script>
+        <script src="app/User/User.js"></script>
+        <script src="app/Students/FormHelper.js"></script>
+        <script src="app/Auth/Auth.js"></script>
+        <script src="app/Auth/Authaid.js"></script>
+        <script src="app/Auth/AuthService.js"></script>
+        <script src="app/Panel.directive.js"></script>
+        <script src="app/HomeController.js"></script>
 
 
         
