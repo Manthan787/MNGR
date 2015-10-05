243e541989c7c45a42aa8c47993aa5406febe5ce
manthan787 <manthant15@gmail.com>
2015-10-05 17:59:26 +0530

    [Admin] Change names

--- public/app/Questions/Questions.js
+++ public/app/Questions/Questions.js
@@ -173,9 +173,9 @@
 					plugins: [
 			        "advlist autolink lists link image charmap print preview anchor",
 			        "searchreplace visualblocks code fullscreen",
-			        "insertdatetime media table contextmenu paste youTube"
+			        "insertdatetime media table contextmenu paste youtube"
 			    ],
-    			toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image youTube",
+    			toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image youtube",
 					file_browser_callback: function(field_name, url, type, win) {
             if(type=='image') $('#my_form input').click();
         	},
